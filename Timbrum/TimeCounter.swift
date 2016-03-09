bf5458466f7c2a894abe9f042386ff2a85e156be
Simone Genovese <genovese@esteco.com>
2016-03-09 08:44:01 +0100

    Added Time Counter Add method

--- Timbrum/TimeCounter.swift
+++ Timbrum/TimeCounter.swift
@@ -11,13 +11,52 @@ import UIKit
 
 
 class TimeCounter {
-  
-    func sum(value : String ){
+    let userCalendar = NSCalendar.currentCalendar()
     
+    var timeToday = NSDate()
+  
+    init(){
+                let mothersDayComponent = NSDateComponents()
+        
+                mothersDayComponent.year = 2016
+        
+                mothersDayComponent.month = 3
+        
+                mothersDayComponent.day = 9
+        
+                mothersDayComponent.hour = 0
+        
+                mothersDayComponent.minute = 0
+        
+                timeToday = userCalendar.dateFromComponents(mothersDayComponent)!
+    }
+    
+    func sum(value : String ){
+
+        
+        
+        let addingPeriod = NSDateComponents()
+        
+        let hours = value.substringWithRange(value.startIndex..<value.startIndex.advancedBy(2))
+        addingPeriod.hour = Int(hours)!
+        
+        let minutes = value.substringWithRange(value.startIndex.advancedBy(2)..<value.startIndex.advancedBy(4))
+        addingPeriod.minute = Int(minutes)!
+        
+        if let newYearsParty = userCalendar.dateByAddingComponents(addingPeriod, toDate: timeToday, options: []) {
+            
+            let dateformatter = NSDateFormatter()
+            
+            dateformatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            
+            dateformatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            
+            print(dateformatter.stringFromDate(newYearsParty))
+        }
     }
     
     func remove( value : String){
-    
+        
     }
     
     func getOreTotali() -> String {
---
+++ TimbrumTests/TimeCounterTest.swift
@@ -0,0 +1,30 @@
+//
+//  TimeCounterTest.swift
+//  Timbrum
+//
+//  Created by Simone Genovese on 09/03/16.
+//  Copyright © 2016 ESTECO. All rights reserved.
+//
+
+import XCTest
+@testable import Timbrum
+
+class TimeCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testTimeAdd() {
+        let timeCounter = TimeCounter()
+        timeCounter.sum("0304")
+    }
+    
+    
+}
